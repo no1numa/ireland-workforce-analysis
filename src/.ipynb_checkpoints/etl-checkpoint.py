44332a1221f0816473739dc37fd78601da1aa5a2
unknown <numaireg@gmail.com>
2025-07-12 15:04:59 +0100

    restructured files and path names + some stuff done in the data ingestion notebook

---
+++ src/.ipynb_checkpoints/etl-checkpoint.py
@@ -0,0 +1,18 @@
+# For extracting and handling data
+import requests
+import time
+import pandas as pd
+from pyjstat import pyjstat
+
+
+#
+def fetch_with_retry(url, retries=5, wait=15):
+    for attempt in range(retries):
+        try:
+            response = requests.get(url, timeout=15)
+            response.raise_for_status()
+            return pyjstat.Dataset.read(response.text)
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            time.sleep(wait)
+    raise Exception("All attempts failed!")
