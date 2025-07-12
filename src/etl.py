46d9274dc5f173a5a616840faa4d71a5937e3ccc
unknown <numaireg@gmail.com>
2025-07-12 16:55:19 +0100

    data exported to .csv

--- src/etl.py
+++ src/etl.py
@@ -6,13 +6,13 @@ from pyjstat import pyjstat
 
 
 #
-def fetch_with_retry(url, retries=5, wait=15):
+def fetch_data(url, retries=5, wait=15):
     for attempt in range(retries):
         try:
             response = requests.get(url, timeout=15)
             response.raise_for_status()
             return pyjstat.Dataset.read(response.text)
         except Exception as e:
-            print(f"Attempt {attempt + 1} failed: {e}")
+            #print(f"Attempt {attempt + 1} failed: {e}")
             time.sleep(wait)
     raise Exception("All attempts failed!")
