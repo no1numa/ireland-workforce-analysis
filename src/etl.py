60570a8fd8cae25c00c6f8d037da3175baa9a392
unknown <numaireg@gmail.com>
2025-08-14 20:30:08 +0100

    trying new functions for the ETL process

--- src/etl.py
+++ src/etl.py
@@ -1,4 +1,4 @@
-# For extracting and handling data
+# Libraries used
 import requests
 import time
 import datetime as dt
@@ -22,3 +22,29 @@ def fetch_data(url, retries=5, wait=15):
 def quarter_to_dt(df):
     df['period'] = pd.PeriodIndex(df['Quarter'], freq='Q')
     df['date'] = df['period'].dt.start_time
+    df.drop(columns=['period', 'Quarter'], inplace=True)
+
+# Putting columns in lower case and without spaces between words
+def col_renaming(df):
+    df.columns = df.columns.map(lambda x: x.lower().replace(' ', '_'))
+
+# Dropping aggregations and null values
+def clean_data(df):
+    df.dropna(subset=['value'], inplace=True)
+    
+    agg_values = ['All NACE economic sectors',
+                 'Both sexes',
+                 'All ILO economic status',
+                 'All employment status',
+                 'State',
+                 'Ireland',
+                 'All nationalities',
+                 'All countries',
+                 'All persons in employment	',
+                 'All marital status',
+                 'All occupational groups']
+
+    #df.drop(agg_values)
+                         
+                         
+    
